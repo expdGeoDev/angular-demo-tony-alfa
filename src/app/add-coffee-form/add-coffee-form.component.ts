47421af5d1544c224143e80b84f08ab64588a018
Antony Alfaro <antony.alfaro@expeditors.com>
2024-06-28 14:04:05 -0600

    Add form fields and validations

--- src/app/add-coffee-form/add-coffee-form.component.ts
+++ src/app/add-coffee-form/add-coffee-form.component.ts
@@ -1,12 +1,50 @@
 import { Component } from '@angular/core';
-
+import {
+	FormBuilder,
+	FormGroup,
+	FormsModule,
+	ReactiveFormsModule,
+	Validators,
+} from '@angular/forms';
+import { NgIf } from '@angular/common';
 @Component({
-  selector: 'app-add-coffee-form',
-  standalone: true,
-  imports: [],
-  templateUrl: './add-coffee-form.component.html',
-  styleUrl: './add-coffee-form.component.css'
+	selector: 'app-add-coffee-form',
+	standalone: true,
+	imports: [FormsModule, ReactiveFormsModule, NgIf],
+	templateUrl: './add-coffee-form.component.html',
+	styleUrl: './add-coffee-form.component.css',
 })
 export class AddCoffeeFormComponent {
+	toastLiveExample = document.getElementById('liveToast');
+	loading: boolean = false;
 
+	constructor(private formBuilder: FormBuilder) {}
+
+	coffeeForm: FormGroup = this.formBuilder.group({
+		brand: ['', Validators.required],
+		coffeeType: ['', Validators.required],
+		roastType: ['', Validators.required],
+		flavorNotes: [''],
+		grind: ['0'],
+	});
+
+	get brand() {
+		return this.coffeeForm.get('brand');
+	}
+
+	get coffeeType() {
+		return this.coffeeForm.get('coffeeType');
+	}
+
+	get roastType() {
+		return this.coffeeForm.get('roastType');
+	}
+
+	async onSubmit() {
+		this.loading = true;
+		await new Promise((f) => setTimeout(f, 1000));
+		this.loading = false;
+		console.log(this.coffeeForm.value);
+		this.coffeeForm.reset();
+	}
 }
